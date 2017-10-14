b9bdf96d95d4865982eebe7a6f309d54ca6eb0d7
jiang <2418952171@qq.com>
2017-10-14 09:53:12 +0800

    添加入口文件

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiang
+ * Date: 2017/10/13
+ * Time: 17:56
+ */
+require "vendor/autoload.php";
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('your.log', Logger::WARNING));
+$log->warn('Foo');
+$log->err('Bar');
